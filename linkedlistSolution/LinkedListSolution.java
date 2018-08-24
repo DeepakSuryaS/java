eec58c08a71987afeec47db67d10ad2e564cc234
DeepakSuryaS <ideepaksuryas@gmail.com>
2018-08-24 20:46:29 +0530

    linked list solution code added

---
+++ linkedlistSolution/LinkedListSolution.java
@@ -0,0 +1,53 @@
+package linkedlistSolution;
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class LinkedListSolution {
+	public static Node insert(Node head,int data) {
+        //Complete this method
+        if(head != null){
+            Node temp = new Node(data);
+            Node current = head;
+            while(current.next != null){
+                current = current.next;
+            }
+            current.next = temp;
+            temp.next = null;
+        } else {
+            head = new Node(data);
+        }
+        return head;
+    }
+    
+    public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
