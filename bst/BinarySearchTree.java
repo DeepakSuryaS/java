b49cb1d3592322bebe604a65ca7bfbedf1a91a26
DeepakSuryaS <ideepaksuryas@gmail.com>
2018-08-27 21:08:05 +0530

    BST implementation added

---
+++ bst/BinarySearchTree.java
@@ -0,0 +1,20 @@
+package bst;
+
+public class BinarySearchTree {
+
+	public static void main (String[] args) {
+		EmptyBST<Integer> emptyTree = new EmptyBST<Integer>();
+		emptyTree.add(9);
+		System.out.println("The integer tree is of type EmptyBST, therefore everthing inside it is null.");
+		System.out.println("Is the integer tree empty : " + emptyTree.isEmpty());
+		System.out.println("Size of integer tree : " + emptyTree.cardinality());
+		System.out.println("Is 6 a member of the integer tree :" + emptyTree.member(6));
+
+		NonEmptyBST<String> stringTree = new NonEmptyBST<String>("Hello");
+		System.out.println("String tree created.");
+		System.out.println("Is the string tree empty : " + stringTree.isEmpty());
+		System.out.println("Size of string tree : " + stringTree.cardinality());
+		System.out.println("Is 'Hello' a member of the string tree : " + stringTree.member("Hello"));
+	}
+
+}
---
+++ bst/Tree.java
@@ -0,0 +1,12 @@
+package bst;
+
+import java.util.*;
+
+public interface Tree<D extends Comparable<D>> {
+
+	public boolean isEmpty();
+	public int cardinality(); //size of the tree
+	public boolean member(D elt);
+	public NonEmptyBST<D> add(D elt);
+
+}
